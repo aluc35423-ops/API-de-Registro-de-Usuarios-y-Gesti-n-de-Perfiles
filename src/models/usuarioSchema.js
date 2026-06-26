4d06124ce8aaf3c8fdc91b9fc5f07dca59e05ffc
alucardo <aluc35423@gmail.com>
2026-06-26 11:36:40 -0600

    Schema

---
+++ src/models/usuarioSchema.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const usuarioSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password:{
+        type: String,
+        required: true
+    },
+    nombre: {
+        type: String,
+        required: true,
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Usuario', usuarioSchema);
